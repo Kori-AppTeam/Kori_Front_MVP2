822b8ad647d7b7d7c9e5ba48bd5b8a0338cec989
usingjun <usingjun99@gmail.com>
2025-11-07 23:58:04 +0900

    fix: icon, 이미지 누락된 부분 추가

--- app/screens/makeprofile/AddPhotoStepScreen.tsx
+++ app/screens/makeprofile/AddPhotoStepScreen.tsx
@@ -218,9 +218,6 @@ export default function AddPhotoStepScreen({}) {
         ...profileWithoutPhoto,
         imageKey,
       };
-
-      console.log("payload"+ payload);
-
       // 서버로 전송
       const res = await api.patch('/api/v1/member/profile/setup', payload);
       router.dismissAll(); // 네비게이션 스택 다 비움
