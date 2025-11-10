9bb334b5756e677579052653f771b63625c697f4
deokjin <ghaehfdl7@naver.com>
2025-11-10 17:52:07 +0900

    feat: api 호출 함수 생성

---
+++ src/features/chat/room/api/messages.ts
@@ -0,0 +1,18 @@
+import api from '@/api/axiosInstance';
+import { ChatMessage } from '../types/chat.types';
+
+//TODO: 에러 처리 필요
+export const loadMessagesAPI = async (roomId: string, lastMessageId: string | number): Promise<ChatMessage[]> => {
+  const res = await api.get(
+    `/api/v1/chat/rooms/${roomId}/messages?lastMessageId=${lastMessageId || ''}`
+  );
+  return res.data.data;
+};
+
+//TODO: 에러 처리 필요
+export const searchMessagesAPI = async (roomId: string, searchText: string): Promise<ChatMessage[]> => {
+  const res = await api.get(
+    `/api/v1/chat/search?roomId=${roomId}&keyword=${encodeURIComponent(searchText)}`
+  );
+  return res.data.data;
+};
