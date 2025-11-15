a09e5d5749eab025079d9db671c8d5079c2d1301
JOEIH <joh73459@gmail.com>
2025-11-16 05:32:46 +0900

    refactor: 게시글 상세 정보 받아오는 훅 생성

---
+++ src/features/community/hooks/useGetPostDetail.ts
@@ -0,0 +1,17 @@
+import { getPostDetail } from '@/src/features/community/apis/post';
+import { useQuery } from '@tanstack/react-query';
+import { PostDetail } from '../types/postsListType';
+
+export function useGetPostDetail(postId: number) {
+  const { data, isLoading, isError } = useQuery<PostDetail>({
+    queryKey: ['post-detail', postId],
+    queryFn: async () => {
+      const data = await getPostDetail(postId);
+      const postDetailData = data.data;
+      return postDetailData;
+    },
+    enabled: !!postId,
+  });
+
+  return { postDetailData: (data ?? {}) as PostDetail, isLoading, isError };
+}
