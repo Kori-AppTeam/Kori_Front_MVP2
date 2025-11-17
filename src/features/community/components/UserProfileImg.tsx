294ebc06ce01237d58d59ba0e112078e8d5f19ae
JOEIH <joh73459@gmail.com>
2025-11-18 01:16:55 +0900

    refactor: 게시글에서 유저 프로필 svg 이미지 처리 로직 추가

--- src/features/community/components/UserProfileImg.tsx
+++ src/features/community/components/UserProfileImg.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { SvgProps } from 'react-native-svg';
+import { SvgProps, SvgUri } from 'react-native-svg';
 import styled from 'styled-components/native';
 
 export default function UserProfileImg({ source }: { source: string | React.FC<SvgProps> }) {
-  if (typeof source === 'string') {
+  if (typeof source !== 'string') {
+    const SVGImage = source;
+    return <SVGImage width="100%" height="100%" />;
+  }
+
+  const extension = source.split('.').pop()?.toLowerCase();
+
+  if (extension !== 'svg') {
     return <ImageString source={{ uri: source }}></ImageString>;
   }
 
-  const SVGImage = source;
-
-  return <SVGImage width="100%" height="100%" />;
+  return <SvgUri uri={source} width="100%" height="100%" />;
 }
 
 // 나중에 react-native 스타일에 맞게 수정 필요
