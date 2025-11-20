7636d0d858f17c371398851d7ee7cff2bdf3854f
JOEIH <joh73459@gmail.com>
2025-11-20 13:40:51 +0900

    refactor: 프로필 이미지 호출 시 memo 사용

--- src/features/community/components/UserProfileImg.tsx
+++ src/features/community/components/UserProfileImg.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { SvgProps, SvgUri } from 'react-native-svg';
 import styled from 'styled-components/native';
 
-export default function UserProfileImg({ source }: { source: string | React.FC<SvgProps> }) {
+function UserProfileImg({ source }: { source: string | React.FC<SvgProps> }) {
   if (typeof source !== 'string') {
     const SVGImage = source;
     return <SVGImage width="100%" height="100%" />;
@@ -11,15 +11,16 @@ export default function UserProfileImg({ source }: { source: string | React.FC<S
   const extension = source.split('.').pop()?.toLowerCase();
 
   if (extension !== 'svg') {
-    return <ImageString source={{ uri: source }}></ImageString>;
+    return <ImageString source={{ uri: source }} resizeMode="cover" />;
   }
 
   return <SvgUri uri={source} width="100%" height="100%" />;
 }
 
-// 나중에 react-native 스타일에 맞게 수정 필요
 const ImageString = styled.Image`
   width: 100%;
   height: 100%;
   border-radius: 50%;
 `;
+
+export default memo(UserProfileImg);
