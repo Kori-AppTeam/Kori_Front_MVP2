4023c83c309ff1ead2530424c89edc36ea4e3854
JOEIH <joh73459@gmail.com>
2025-11-20 14:05:01 +0900

    chore: scrollref 타입 추가

--- src/features/community/hooks/useScrollToTop.tsx
+++ src/features/community/hooks/useScrollToTop.tsx
@@ -1,8 +1,9 @@
 import { useCallback, useRef } from 'react';
 import { FlatList } from 'react-native';
+import { PostsListItem } from '../types/postsListType';
 
 export default function useScrollToTop() {
-  const scrollRef = useRef<FlatList>(null);
+  const scrollRef = useRef<FlatList<PostsListItem>>(null);
 
   const scrollToTop = useCallback((animated: boolean) => {
     if (scrollRef.current) {
