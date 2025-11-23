949e6040f867c08eda573ed838a49ad4f474cd1e
JOEIH <joh73459@gmail.com>
2025-11-24 03:15:04 +0900

    refactor: 게시글 전체 리스트 파일에서 좋아요, 북마크 로직 삭제

--- src/features/community/components/PostList.tsx
+++ src/features/community/components/PostList.tsx
@@ -3,8 +3,6 @@ import React, { RefObject } from 'react';
 import { ActivityIndicator, FlatList, ListRenderItem } from 'react-native';
 import styled from 'styled-components/native';
 import { useGetPosts } from '../hooks/useGetPosts';
-import { useToggleBookmark } from '../hooks/useToggleBookmark';
-import { useToggleLike } from '../hooks/useToggleLike';
 import { AllowedCategory, PostsListItem, SortParam } from '../types/postsListType';
 import PostListCard from './PostListCard';
 
@@ -18,23 +16,8 @@ const PostList = ({ category, sort, scrollRef }: Props) => {
   const { data, posts, isLoading, isFetchingNextPage, isError, hasNextPage, refetch, isRefetching, fetchNextPage } =
     useGetPosts(CATEGORY_TO_BOARD_ID[category], sort);
 
-  const likeMutation = useToggleLike(CATEGORY_TO_BOARD_ID[category], sort);
-  const bookmarkMutation = useToggleBookmark(CATEGORY_TO_BOARD_ID[category], sort);
-
-  const handleToggleLike = (postId: number, isLike: boolean) => {
-    likeMutation.mutate({ postId: postId, liked: isLike });
-  };
-
-  const handleToggleBookmark = (postId: number, isBookmark: boolean) => {
-    bookmarkMutation.mutate({ postId: postId, isBookmarked: isBookmark });
-  };
-
   const renderPost: ListRenderItem<PostsListItem> = ({ item }) => (
-    <PostListCard
-      data={item}
-      onToggleBookmark={() => handleToggleBookmark(item.postId, item.isBookmarked)}
-      onToggleLike={() => handleToggleLike(item.postId, item.isLiked)}
-    />
+    <PostListCard data={item} sort={sort} category={category} />
   );
 
   return (
