ba75366d617f443d91136854085c01117ed8f022
deokjin <ghaehfdl7@naver.com>
2025-11-26 17:16:32 +0900

    feat: 공통 컴포넌트 속성 수정
    - 상단 핸들러 null
    - 배경색 커스텀 props 추가

--- src/shared/components/CustomBottomSheet.tsx
+++ src/shared/components/CustomBottomSheet.tsx
@@ -1,20 +1,31 @@
-import React, { ReactElement, useCallback } from 'react';
-import { BottomSheetBackdrop, BottomSheetBackdropProps, BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet';
 import { theme } from '@/src/styles/theme';
+import { BottomSheetBackdrop, BottomSheetBackdropProps, BottomSheetModal, BottomSheetView } from '@gorhom/bottom-sheet';
+import React, { ReactElement, useCallback } from 'react';
 
 interface CustomBottomSheetProps {
   children: ReactElement;
   ref: React.RefObject<BottomSheetModal | null>;
   onChange?: (index: number) => void;
+  backgroundColor?: string;
 }
 
-const CustomBottomSheet = ({ children, ref, onChange }: CustomBottomSheetProps) => {
+const CustomBottomSheet = ({ children, ref, onChange, backgroundColor }: CustomBottomSheetProps) => {
   const renderBackdrop = useCallback(
     (props: BottomSheetBackdropProps) => (
       <BottomSheetBackdrop {...props} disappearsOnIndex={-1} appearsOnIndex={0} opacity={0.6} pressBehavior="close" />
     ),
     [],
   );
+
+  const bottomSheetModalStyle = {
+    backgroundColor: backgroundColor ?? theme.colors.gray.darkGray_1,
+  } as const;
+
+  const bottomSheetHandleStyle = {
+    backgroundColor: backgroundColor ?? theme.colors.gray.gray_1,
+  } as const;
+
+  const bottomSheetViewStyle = { flex: 1, backgroundColor: backgroundColor ?? theme.colors.gray.darkGray_1 } as const;
 
   return (
     <BottomSheetModal
@@ -23,20 +34,11 @@ const CustomBottomSheet = ({ children, ref, onChange }: CustomBottomSheetProps) 
       backgroundStyle={bottomSheetModalStyle}
       handleIndicatorStyle={bottomSheetHandleStyle}
       backdropComponent={renderBackdrop}
+      handleComponent={() => null} // 핸들 숨기기
     >
       <BottomSheetView style={bottomSheetViewStyle}>{children}</BottomSheetView>
     </BottomSheetModal>
   );
 };
-
-const bottomSheetModalStyle = {
-  backgroundColor: theme.colors.gray.darkGray_1,
-} as const;
-
-const bottomSheetHandleStyle = {
-  backgroundColor: theme.colors.gray.gray_1,
-} as const;
-
-const bottomSheetViewStyle = { flex: 1, backgroundColor: theme.colors.gray.darkGray_1 } as const;
 
 export default CustomBottomSheet;
