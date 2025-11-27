2f9caeac41a57b509388bb38528ec2b549e4e2fa
JOEIH <joh73459@gmail.com>
2025-11-28 04:45:42 +0900

    refactor: 게시글 더보기 버튼 클릭 시 내 게시글인지 확인하는 로직 추가

--- src/features/community/hooks/useOpenMoreSheet.ts
+++ src/features/community/hooks/useOpenMoreSheet.ts
@@ -1,21 +1,28 @@
 import { BottomSheetModal } from '@gorhom/bottom-sheet';
-import { useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import useGetVisitor from './useGetVisitor';
 
 // 게시글 더보기 모달 열기 훅
 export const useOpenMoreSheet = () => {
   const [selectedPost, setSelectedPost] = useState<null | number>(null);
+  const [isMine, setIsMine] = useState<boolean>(false);
   const bottomSheetRef = useRef<BottomSheetModal>(null);
-  const { data } = useGetVisitor();
+  const { data } = useGetVisitor(); // 내 id 가져오기
 
-  const isMyPost = (authorId: number) => {
-    return data?.userId === authorId ? true : false;
-  };
-  const openModal = (postId: number, authorId: number) => {
-    if (selectedPost === postId) return; // 이미 열려있는 모달이면 무시
-    setSelectedPost(postId);
-    bottomSheetRef.current?.present();
-  };
+  const openModal = useCallback(
+    (postId: number, authorId: number) => {
+      setSelectedPost(postId);
+
+      // 내 게시글인지 확인
+      if (data) {
+        setIsMine(data.userId === authorId);
+      }
+
+      bottomSheetRef.current?.present();
+    },
+    [data],
+  );
+
   const closeModal = () => {
     setSelectedPost(null);
     bottomSheetRef.current?.dismiss();
@@ -23,7 +30,7 @@ export const useOpenMoreSheet = () => {
 
   return {
     selectedPost,
-    isMyPost,
+    isMine,
     openModal,
     closeModal,
     bottomSheetRef,
