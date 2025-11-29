7a4a4c4b5d34d56900d445a6c1d196c7e7933f80
JOEIH <joh73459@gmail.com>
2025-11-30 05:46:31 +0900

    refactor: 전체 게시글 호출 로직 개선

--- src/features/community/hooks/useGetPosts.ts
+++ src/features/community/hooks/useGetPosts.ts
@@ -1,21 +1,20 @@
 import { useInfiniteQuery } from '@tanstack/react-query';
+import { useMemo } from 'react';
 import { getPosts } from '../apis/post';
 import { BoardId, PostsCursorPage, PostsListResp, SortParam } from '../types';
 
 export function useGetPosts(boardId: BoardId, sort: SortParam) {
-  const { data, isLoading, isFetchingNextPage, isError, hasNextPage, refetch, isRefetching, fetchNextPage } =
-    useInfiniteQuery<PostsListResp>({
-      queryKey: ['post-list', boardId, sort],
-      initialPageParam: undefined,
-      queryFn: ({ pageParam }) => getPosts(boardId, { sort: sort, size: 20, cursor: pageParam as string | undefined }),
-      getNextPageParam: (lastItem) => {
-        const item: PostsCursorPage = lastItem.data;
+  const query = useInfiniteQuery<PostsListResp>({
+    queryKey: ['post-list', boardId, sort],
+    initialPageParam: undefined,
+    queryFn: ({ pageParam }) => getPosts(boardId, { sort: sort, size: 20, cursor: pageParam as string | undefined }),
+    getNextPageParam: (lastItem) => {
+      const item: PostsCursorPage = lastItem.data;
+      return item.hasNext ? (item.nextCursor ?? undefined) : undefined;
+    },
+  });
 
-        return item.hasNext ? (item.nextCursor ?? undefined) : undefined;
-      },
-    });
+  const posts = useMemo(() => query.data?.pages.flatMap((p) => p.data.items) ?? [], [query.data]);
 
-  const posts = data?.pages.flatMap((p) => p.data.items) ?? [];
-
-  return { data, posts, isLoading, isFetchingNextPage, isError, hasNextPage, refetch, isRefetching, fetchNextPage };
+  return { posts, ...query };
 }
