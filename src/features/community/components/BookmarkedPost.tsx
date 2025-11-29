aa65b995a7d932a002ee8540dd64a00a687d0a2d
JOEIH <joh73459@gmail.com>
2025-11-30 06:56:11 +0900

    feat: 북마크 단일 게시글 컴포넌트 생성

---
+++ src/features/community/components/BookmarkedPost.tsx
@@ -0,0 +1,161 @@
+import Icon from '@/components/common/Icon';
+import { textStyle } from '@/src/styles/theme';
+import { router } from 'expo-router';
+import React, { memo } from 'react';
+import { Dimensions } from 'react-native';
+import styled from 'styled-components/native';
+import { useHandleLikeBookmark } from '../hooks/useHandleLikeBookmark';
+import { BookmarkedPostItem } from '../types';
+import { timeToAgo } from '../utils/indexUtils';
+import PostBookmarkButton from './post/PostBookmarkButton';
+import PostComment from './post/PostComment';
+import PostLikeButton from './post/PostLikeButton';
+import PostTextContent from './post/PostTextContent';
+import PostUserProfileImg from './post/PostUserProfileImg';
+
+const BookmarkedPost = ({
+  data,
+  onOpenModal,
+}: {
+  data: BookmarkedPostItem;
+  onOpenModal: (postId: number, authorId: number) => void;
+}) => {
+  const SCREEN_WIDTH = Math.round(Dimensions.get('window').width);
+
+  const { handleToggleLike, handleToggleBookmark } = useHandleLikeBookmark();
+
+  return (
+    <Container width={SCREEN_WIDTH}>
+      <Wrap
+        width={SCREEN_WIDTH}
+        onPress={() => router.push({ pathname: '/community/[id]', params: { id: data.postId } })}
+      >
+        <PostHeader>
+          <AuthorImageContainer>
+            <PostUserProfileImg isAnonymous={data.isAnonymous} userImageUrl={data.userImage} />
+          </AuthorImageContainer>
+
+          <Meta>
+            <Author>{data.authorName}</Author>
+            <SubRow>
+              <TimeText>{timeToAgo(data.createdAt)}</TimeText>
+              <Dot>•</Dot>
+              <IconBtn>
+                <Icon size={16} type="eye" color="#848687" />
+                <SmallCount>{data.checkCount}</SmallCount>
+              </IconBtn>
+            </SubRow>
+          </Meta>
+
+          <PostBookmarkButton
+            isBookmarked={data.isBookmarked}
+            onToggleBookmark={() => handleToggleBookmark(data.postId, data.isBookmarked)}
+          />
+        </PostHeader>
+
+        <ContentBox>
+          <PostTextContent isTruncate={true} postId={data.postId} content={data.content} />
+        </ContentBox>
+
+        <FooterRow>
+          <LeftFooter>
+            <PostLikeButton
+              isLiked={data.isLiked}
+              likeCount={data.likeCount}
+              onToggleLike={() => handleToggleLike(data.postId, data.isLiked)}
+            />
+
+            <PostComment showComment={false} postId={data.postId} commentCount={data.commentCount} />
+          </LeftFooter>
+
+          <IconBtn onPress={() => onOpenModal(data.postId, data.authorId)}>
+            <Icon type="eclipsisGaro" size={20} />
+          </IconBtn>
+        </FooterRow>
+      </Wrap>
+      <BorderLine width={SCREEN_WIDTH} />
+    </Container>
+  );
+};
+
+export default memo(BookmarkedPost);
+
+const Container = styled.View<{ width: number }>`
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const Wrap = styled.Pressable<{ width: number }>`
+  width: ${({ width }) => (width ? width - 20 * 2 : 335)};
+  padding: 20px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const BorderLine = styled.View<{ width: number }>`
+  width: ${({ width }) => (width ? width - 20 * 2 : 335)}px;
+  border-bottom-color: ${({ theme }) => theme.colors.gray.darkGray_1};
+  border-bottom-width: 1px;
+`;
+const PostHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+const AuthorImageContainer = styled.View`
+  width: 44px;
+  height: 44px;
+`;
+const Meta = styled.View`
+  margin-left: 8px;
+  flex: 1;
+  gap: 4px;
+  flex-direction: column;
+  justify-content: start;
+`;
+const Author = styled.Text`
+  color: ${({ theme }) => theme.colors.primary.white};
+  ${({ theme }) => textStyle(theme.fonts.body.B4_SB)}
+`;
+const SubRow = styled.View`
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+`;
+const TimeText = styled.Text`
+  color: ${({ theme }) => theme.colors.gray.gray_1};
+  font-size: 11px;
+  ${({ theme }) => textStyle(theme.fonts.small.small_M)}
+`;
+const Dot = styled.Text`
+  color: ${({ theme }) => theme.colors.gray.gray_1};
+`;
+const SmallCount = styled.Text`
+  ${({ theme }) => textStyle(theme.fonts.small.small_M)}
+  color: ${({ theme }) => theme.colors.gray.gray_1};
+  font-size: 11px;
+  margin-left: 4px;
+`;
+const ContentBox = styled.View`
+  width: 100%;
+  padding: 20px 0;
+  flex-direction: column;
+  justify-content: center;
+  gap: 16px;
+`;
+const FooterRow = styled.View`
+  width: 100%;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const LeftFooter = styled.View`
+  flex-direction: row;
+  align-items: center;
+  row-gap: 16px;
+`;
+const IconBtn = styled.Pressable`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
