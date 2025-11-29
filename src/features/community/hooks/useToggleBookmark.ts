a9eed075da773f543aaf5f5653b1070518fa7818
JOEIH <joh73459@gmail.com>
2025-11-30 05:49:47 +0900

    refactor: 북마크 훅 로직 개선(북마크 리스트에서도 낙관적 업데이트 반영)

--- src/features/community/hooks/useToggleBookmark.ts
+++ src/features/community/hooks/useToggleBookmark.ts
@@ -1,63 +1,96 @@
-import { InfiniteData, useMutation, useQueryClient } from '@tanstack/react-query';
+import { InfiniteData, QueryClient, QueryKey, useMutation, useQueryClient } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
 import { toggleBookMark } from '../apis/bookmarks';
-import { BoardId, PostDetail, PostsListItem, PostsListResp, SortParam } from '../types';
+import { BookmarkedPostsResp, PostDetail, PostsListResp } from '../types';
 
 type Vars = { postId: number; isBookmarked: boolean };
-type Context = { prevListData?: InfiniteData<PostsListResp>; prevDetailData?: PostDetail };
+type Context = {
+  prevListData?: Array<[QueryKey, InfiniteData<PostsListResp> | undefined]>;
+  prevDetailData?: PostDetail;
+  prevBookmarkData?: Array<[QueryKey, InfiniteData<BookmarkedPostsResp> | undefined]>;
+};
 
-export function useToggleBookmark(boardId: BoardId, sort: SortParam) {
+export function useToggleBookmark() {
   const qc = useQueryClient();
 
   return useMutation<boolean, AxiosError, Vars, Context>({
     mutationFn: ({ postId, isBookmarked }) => toggleBookMark(postId, isBookmarked),
     onMutate: async ({ postId, isBookmarked }): Promise<Context> => {
-      await qc.cancelQueries({ queryKey: ['post-list'] });
-      await qc.cancelQueries({ queryKey: ['post-detail', postId] });
+      await Promise.all([
+        qc.cancelQueries({ queryKey: ['post-list'] }),
+        qc.cancelQueries({ queryKey: ['bookmarked-posts'] }),
+        qc.cancelQueries({ queryKey: ['post-detail', postId] }),
+      ]);
 
-      const prevListData = qc.getQueryData<InfiniteData<PostsListResp>>(['post-list', boardId, sort]);
+      const prevListData = qc.getQueriesData({ queryKey: ['post-list'] }) as Array<
+        [QueryKey, InfiniteData<PostsListResp>]
+      >;
+      const prevBookmarkData = qc.getQueriesData({ queryKey: ['bookmarked-posts'] }) as Array<
+        [QueryKey, InfiniteData<BookmarkedPostsResp>]
+      >;
       const prevDetailData = qc.getQueryData<PostDetail>(['post-detail', postId]);
 
-      // 전체 리스트
-      qc.setQueriesData({ queryKey: ['post-list'] }, (oldListData: InfiniteData<PostsListResp>) => {
-        if (!oldListData) return [];
+      updateBookmarkListCache(qc, ['post-list'], postId, isBookmarked);
+      updateBookmarkDetailCache(qc, postId, isBookmarked);
+      updateBookmarkListCache(qc, ['bookmarked-posts'], postId, isBookmarked);
 
-        const newPage = oldListData.pages.map((page) => {
-          const newData: PostsListItem[] = page.data.items.map((item) => {
-            return item.postId === postId ? { ...item, isBookmarked: !isBookmarked } : item;
-          });
-          return {
-            ...page,
-            data: {
-              ...page.data,
-              items: newData,
-            },
-          };
-        });
-        return {
-          ...oldListData,
-          pages: newPage,
-        };
-      });
-
-      //상세 게시글
-      qc.setQueryData(['post-detail', postId], (oldData: PostDetail) => {
-        if (!oldData) return undefined;
-
-        return { ...oldData, isBookmarked: !isBookmarked };
-      });
-
-      return { prevListData, prevDetailData };
+      return { prevListData, prevDetailData, prevBookmarkData };
     },
     onError: (error, { postId, isBookmarked }, context) => {
       if (context?.prevDetailData) {
         qc.setQueryData(['post-detail', postId], context.prevDetailData);
       }
       if (context?.prevListData) {
-        qc.setQueriesData({ queryKey: ['post-list', boardId, sort] }, context.prevListData);
-      } else {
-        qc.invalidateQueries({ queryKey: ['post-list'] });
+        context.prevListData.forEach(([queryKey, data]) => {
+          qc.setQueryData(queryKey, data);
+        });
+      }
+      if (context?.prevBookmarkData) {
+        context.prevBookmarkData.forEach(([queryKey, data]) => {
+          qc.setQueryData(queryKey, data);
+        });
+      }
+    },
+    onSettled: (data, errors, { isBookmarked }) => {
+      // 북마크 해제 시 북마크 페이지 안에 있으면 refetch 하지 않고 데이터가 상했다는 표시만 전달
+      if (!isBookmarked) {
+        qc.invalidateQueries({ queryKey: ['bookmarked-posts'], refetchType: 'none' });
       }
     },
   });
 }
+
+// 커뮤니티 북마크 게시글 리스트 캐시 업데이트
+const updateBookmarkListCache = (
+  qc: QueryClient,
+  selectedQueryKey: QueryKey,
+  postId: number,
+  isBookmarked?: boolean,
+) => {
+  qc.setQueriesData({ queryKey: selectedQueryKey }, (oldListData: InfiniteData<BookmarkedPostsResp>) => {
+    if (!oldListData) return [];
+
+    return {
+      ...oldListData,
+      pages: oldListData.pages.map((page) => {
+        return {
+          ...page,
+          data: {
+            ...page.data,
+            items: page.data.items.map((item) => {
+              return item.postId === postId ? { ...item, isBookmarked: !isBookmarked } : item;
+            }),
+          },
+        };
+      }),
+    };
+  });
+};
+
+// 상세 게시글 캐시 업데이트
+const updateBookmarkDetailCache = (qc: QueryClient, postId: number, isBookmarked: boolean) => {
+  qc.setQueryData(['post-detail', postId], (oldData: PostDetail) => {
+    if (!oldData) return undefined;
+    return { ...oldData, isBookmarked: !isBookmarked };
+  });
+};
