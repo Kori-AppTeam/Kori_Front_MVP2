c07e4189dfdb7044f7e31aa3253832913a7c1ff1
shinhm1 <shinhm1@naver.com>
2025-12-05 03:04:45 +0900

    feat: 앱 버전 체크 및 앱 버전 에러 Alert 유틸 함수 구현

--- src/shared/utils/showUpdateAlert.ts
+++ src/shared/utils/showUpdateAlert.ts
@@ -1,3 +1,5 @@
+import { APP_VERSION_CHECK_ERROR } from '@/src/shared/constants/error';
+import { getAxiosErrorCode } from '@/src/shared/utils/getAxiosErrorCode';
 import { Alert, Linking } from 'react-native';
 
 export function showUpdateAlert(title: string, message: string, storeUrl: string, isForce: boolean) {
@@ -17,4 +19,17 @@ export function showUpdateAlert(title: string, message: string, storeUrl: string
     ],
     { cancelable: !isForce },
   );
+}
+
+export function showUpdateErrorAlert(error: unknown) {
+  const errorConfig = getAxiosErrorCode(error);
+
+  const title = 'Version Error';
+  const message = APP_VERSION_CHECK_ERROR[errorConfig].message;
+  Alert.alert(title, message, [
+    {
+      text: 'OK',
+      onPress: () => {},
+    },
+  ]);
 }
