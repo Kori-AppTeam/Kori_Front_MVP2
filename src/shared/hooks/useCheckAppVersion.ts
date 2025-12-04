b4996124bb4448492dfeab479ac26788d5b5c8a9
shinhm1 <shinhm1@naver.com>
2025-12-05 03:03:19 +0900

    feat: 함수명 수정, 상태 및 에러 핸들링 Alert 추가

--- src/shared/hooks/useCheckAppVersion.ts
+++ src/shared/hooks/useCheckAppVersion.ts
@@ -1,10 +1,13 @@
 import { Platform } from 'react-native';
 import Constants from 'expo-constants';
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { getAppVersion } from '@/src/shared/api/getAppVersion';
-import { showUpdateAlert } from '@/src/shared/utils/showUpdateAlert';
+import { showUpdateAlert, showUpdateErrorAlert } from '@/src/shared/utils/showUpdateAlert';
 
-export function useAppVersion() {
+export function useCheckAppVersion() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isAppUpToDate, setIsAppUpToDate] = useState<boolean>(false);
+
   const currentVersion = Constants.expoConfig?.version;
   const platform = Platform.OS;
 
@@ -15,32 +18,38 @@ export function useAppVersion() {
         throw new Error('APP_VERSION_NOT_FOUND'); // 앱 버전 정보를 찾을 수 없는 경우 error 처리
       }
       const { status, title, message, storeUrl } = await getAppVersion(platform, currentVersion);
-
       switch (status) {
         // 강제 업데이트 Alert
         case 'FORCE_UPDATE':
-          console.log('force update');
           showUpdateAlert(title, message, storeUrl, true);
+          setIsAppUpToDate(false);
           return;
 
         // 권장 업데이트 Alert
         case 'RECOMMEND_UPDATE':
-          console.log('recommend update');
           showUpdateAlert(title, message, storeUrl, false);
+          setIsAppUpToDate(false);
           return;
 
         // 업데이트 필요 없음
         case 'PASS':
-          console.log('✨✨✨✨✨✨✨✨✨✨✨✨✨pass');
+          setIsAppUpToDate(true);
           return;
 
         default:
           throw new Error('UNKNOWN_STATUS'); // 알 수 없는 status인 경우 error 처리
       }
-    } catch (error) {
-      throw error;
+    } catch (error: unknown) {
+      showUpdateErrorAlert(error); // 에러 Alert 표시
+    } finally {
+      setIsLoading(false);
     }
   }, []);
 
-  return { checkAppVersion };
+  useEffect(() => {
+    // 앱 버전 확인
+    checkAppVersion();
+  }, []);
+
+  return { isLoading, isAppUpToDate };
 }
