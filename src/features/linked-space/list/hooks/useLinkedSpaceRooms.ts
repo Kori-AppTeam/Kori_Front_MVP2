c9fdac5c05b4a68711e0d885d4b911a7d0ba0377
deokjin <ghaehfdl7@naver.com>
2025-12-06 09:00:39 +0900

    refactor: 쿼리키 export 및 적용

--- src/features/linked-space/list/hooks/useLinkedSpaceRooms.ts
+++ src/features/linked-space/list/hooks/useLinkedSpaceRooms.ts
@@ -2,16 +2,19 @@ import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 import { fetchAllSpaces, fetchBuzzingSpaces } from '../api/linkedSpace';
 import { LinkedSpace } from '../types';
 
+export const BUZZING_SPACES_QUERY_KEY = ['buzzingSpaces'];
+export const ALL_SPACES_QUERY_KEY = ['allSpaces'];
+
 export const useBuzzingSpaces = () => {
   return useQuery<LinkedSpace[]>({
-    queryKey: ['buzzingSpaces'],
+    queryKey: BUZZING_SPACES_QUERY_KEY,
     queryFn: fetchBuzzingSpaces,
   });
 };
 
 export const useAllSpaces = () => {
   return useInfiniteQuery<LinkedSpace[]>({
-    queryKey: ['allSpaces'],
+    queryKey: ALL_SPACES_QUERY_KEY,
     queryFn: ({ pageParam }) => fetchAllSpaces(pageParam as number | null),
     initialPageParam: null,
     getNextPageParam: (lastPage) => {
