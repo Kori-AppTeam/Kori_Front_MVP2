34e0f0c473f672ff3eff2d7130e69a8de91c0277
JOEIH <joh73459@gmail.com>
2025-12-08 00:47:24 +0900

    refactor: 북마크 리스트에서 게시글 공통 헤더 컴포넌트 사용하여 기존 코드 대체

--- src/features/community/post/components/BookmarkedPost.tsx
+++ src/features/community/post/components/BookmarkedPost.tsx
@@ -1,18 +1,15 @@
 import Icon from '@/components/common/Icon';
 import { COMMUNITY_ROUTER } from '@/src/shared/constants/route';
-import { textStyle } from '@/src/styles/theme';
 import { router } from 'expo-router';
 import React, { memo } from 'react';
 import { Dimensions } from 'react-native';
 import styled from 'styled-components/native';
-import { timeToAgo } from '../../shared/utils/indexUtils';
 import { useHandleLikeBookmark } from '../hooks/useHandleLikeBookmark';
 import { BookmarkedPostItem } from '../types';
 import PostTextContent from './elements/body/PostTextContent';
 import PostComment from './elements/footer/PostComment';
 import PostLikeButton from './elements/footer/PostLikeButton';
-import PostBookmarkButton from './elements/header/PostBookmarkButton';
-import PostUserProfileImg from './elements/header/PostUserProfileImg';
+import PostCommonHeader from './elements/header/PostCommonHeader';
 
 const BookmarkedPost = ({
   data,
@@ -31,28 +28,16 @@ const BookmarkedPost = ({
         width={SCREEN_WIDTH}
         onPress={() => router.push({ pathname: COMMUNITY_ROUTER.DETAIL, params: { id: data.postId } })}
       >
-        <PostHeader>
-          <AuthorImageContainer>
-            <PostUserProfileImg isAnonymous={data.isAnonymous} userImageUrl={data.userImage} />
-          </AuthorImageContainer>
-
-          <Meta>
-            <Author>{data.authorName}</Author>
-            <SubRow>
-              <TimeText>{timeToAgo(data.createdAt)}</TimeText>
-              <Dot>•</Dot>
-              <IconBtn>
-                <Icon size={16} type="eye" color="#848687" />
-                <SmallCount>{data.checkCount}</SmallCount>
-              </IconBtn>
-            </SubRow>
-          </Meta>
-
-          <PostBookmarkButton
-            isBookmarked={data.isBookmarked}
-            onToggleBookmark={() => handleToggleBookmark(data.postId, data.isBookmarked)}
-          />
-        </PostHeader>
+        <PostCommonHeader
+          postId={data.postId}
+          isAnonymous={data.isAnonymous}
+          userImageUrl={data.userImage}
+          authorName={data.authorName}
+          createdAt={data.createdAt}
+          viewCount={data.checkCount}
+          isBookmarked={data.isBookmarked}
+          onToggleBookmark={() => handleToggleBookmark(data.postId, data.isBookmarked)}
+        />
 
         <ContentBox>
           <PostTextContent isTruncate={true} postId={data.postId} content={data.content} />
@@ -98,44 +83,6 @@ const BorderLine = styled.View<{ width: number }>`
   width: ${({ width }) => (width ? width - 20 * 2 : 335)}px;
   border-bottom-color: ${({ theme }) => theme.colors.gray.darkGray_1};
   border-bottom-width: 1px;
-`;
-const PostHeader = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-const AuthorImageContainer = styled.View`
-  width: 44px;
-  height: 44px;
-`;
-const Meta = styled.View`
-  margin-left: 8px;
-  flex: 1;
-  gap: 4px;
-  flex-direction: column;
-  justify-content: start;
-`;
-const Author = styled.Text`
-  color: ${({ theme }) => theme.colors.primary.white};
-  ${({ theme }) => textStyle(theme.fonts.body.B4_SB)}
-`;
-const SubRow = styled.View`
-  flex-direction: row;
-  align-items: center;
-  gap: 5px;
-`;
-const TimeText = styled.Text`
-  color: ${({ theme }) => theme.colors.gray.gray_1};
-  font-size: 11px;
-  ${({ theme }) => textStyle(theme.fonts.small.small_M)}
-`;
-const Dot = styled.Text`
-  color: ${({ theme }) => theme.colors.gray.gray_1};
-`;
-const SmallCount = styled.Text`
-  ${({ theme }) => textStyle(theme.fonts.small.small_M)}
-  color: ${({ theme }) => theme.colors.gray.gray_1};
-  font-size: 11px;
-  margin-left: 4px;
 `;
 const ContentBox = styled.View`
   width: 100%;
