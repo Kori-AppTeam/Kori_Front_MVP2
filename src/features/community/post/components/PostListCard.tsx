bab098a45c4b33e9870cc9e7d5ccede3c955f2ce
JOEIH <joh73459@gmail.com>
2025-12-08 00:46:48 +0900

    refactor: 게시글 전체 리스트의 게시글 카드에서 공통 헤더 컴포넌트 사용하여 기존 코드 대체

--- src/features/community/post/components/PostListCard.tsx
+++ src/features/community/post/components/PostListCard.tsx
@@ -1,12 +1,9 @@
 import Icon from '@/components/common/Icon';
 import { COMMUNITY_ROUTER } from '@/src/shared/constants/route';
-import { textStyle } from '@/src/styles/theme';
 import { router } from 'expo-router';
 import React, { memo } from 'react';
 import { Dimensions } from 'react-native';
 import styled from 'styled-components/native';
-import { CLIENT_CATEGORY_NAME } from '../../shared/constants/constants';
-import { timeToAgo } from '../../shared/utils/indexUtils';
 import { useHandleLikeBookmark } from '../hooks/useHandleLikeBookmark';
 import useVisitor from '../hooks/useVisitor';
 import { PostsListItem } from '../types';
@@ -14,8 +11,7 @@ import PostSingleImage from './elements/body/PostSingleImage';
 import PostTextContent from './elements/body/PostTextContent';
 import PostComment from './elements/footer/PostComment';
 import PostLikeButton from './elements/footer/PostLikeButton';
-import PostBookmarkButton from './elements/header/PostBookmarkButton';
-import PostUserProfileImg from './elements/header/PostUserProfileImg';
+import PostCommonHeader from './elements/header/PostCommonHeader';
 
 type PostCardProps = {
   data: PostsListItem;
@@ -36,31 +32,17 @@ const PostListCard = ({ data, onOpenModal }: PostCardProps) => {
           handleBlockVisitor(() => router.push({ pathname: COMMUNITY_ROUTER.DETAIL, params: { id: data.postId } }));
         }}
       >
-        <PostHeader>
-          <AuthorImageContainer>
-            <PostUserProfileImg isAnonymous={data.isAnonymous} userImageUrl={data.userImageUrl} />
-          </AuthorImageContainer>
-
-          <Meta>
-            <Author>{data.authorName}</Author>
-            <SubRow>
-              <TimeText>{timeToAgo(data.createdAt)}</TimeText>
-              <CategoryBadge>
-                <CategoryText>{CLIENT_CATEGORY_NAME[data.boardCategory]}</CategoryText>
-              </CategoryBadge>
-              <Dot>•</Dot>
-              <IconBtn>
-                <Icon size={16} type="eye" color="#848687" />
-                <SmallCount>{data.viewCount}</SmallCount>
-              </IconBtn>
-            </SubRow>
-          </Meta>
-
-          <PostBookmarkButton
-            isBookmarked={data.isBookmarked}
-            onToggleBookmark={() => handleToggleBookmark(data.postId, data.isBookmarked)}
-          />
-        </PostHeader>
+        <PostCommonHeader
+          postId={data.postId}
+          isAnonymous={data.isAnonymous}
+          userImageUrl={data.userImageUrl}
+          authorName={data.authorName}
+          createdAt={data.createdAt}
+          boardCategory={data.boardCategory}
+          viewCount={data.viewCount}
+          isBookmarked={data.isBookmarked}
+          onToggleBookmark={() => handleBlockVisitor(() => handleToggleBookmark(data.postId, data.isBookmarked))}
+        />
 
         <ContentBox>
           {data.contentImageUrl && (
@@ -114,55 +96,6 @@ const BorderLine = styled.View<{ width: number }>`
   width: ${({ width }) => (width ? width - 20 * 2 : 335)}px;
   border-bottom-color: ${({ theme }) => theme.colors.gray.darkGray_1};
   border-bottom-width: 1px;
-`;
-const PostHeader = styled.View`
-  flex-direction: row;
-  align-items: center;
-`;
-const AuthorImageContainer = styled.View`
-  width: 44px;
-  height: 44px;
-`;
-const Meta = styled.View`
-  margin-left: 8px;
-  flex: 1;
-  gap: 4px;
-  flex-direction: column;
-  justify-content: start;
-`;
-const Author = styled.Text`
-  color: ${({ theme }) => theme.colors.primary.white};
-  ${({ theme }) => textStyle(theme.fonts.body.B4_SB)}
-`;
-const SubRow = styled.View`
-  flex-direction: row;
-  align-items: center;
-  gap: 5px;
-`;
-const TimeText = styled.Text`
-  color: ${({ theme }) => theme.colors.gray.gray_1};
-  font-size: 11px;
-  ${({ theme }) => textStyle(theme.fonts.small.small_M)}
-`;
-const CategoryBadge = styled.View`
-  padding: 4px 6px;
-  align-items: center;
-  justify-content: center;
-  border-radius: 4px;
-  background: #184b3f;
-`;
-const CategoryText = styled.Text`
-  color: ${({ theme }) => theme.colors.gray.lightGray_1};
-  ${({ theme }) => textStyle(theme.fonts.small.small_M)}
-`;
-const Dot = styled.Text`
-  color: ${({ theme }) => theme.colors.gray.gray_1};
-`;
-const SmallCount = styled.Text`
-  ${({ theme }) => textStyle(theme.fonts.small.small_M)}
-  color: ${({ theme }) => theme.colors.gray.gray_1};
-  font-size: 11px;
-  margin-left: 4px;
 `;
 const ContentBox = styled.View`
   width: 100%;
