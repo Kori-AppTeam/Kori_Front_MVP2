be97c1b0ae7bf6e6b6cd0765723573826bc57801
JOEIH <joh73459@gmail.com>
2025-12-11 00:17:14 +0900

    feat: 더보기 모달 컴포넌트 분리

---
+++ src/features/community/post/components/elements/footer/MoreBottomSheet.tsx
@@ -0,0 +1,47 @@
+import CustomBottomSheet from '@/src/shared/components/CustomBottomSheet';
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+import React, { useEffect, useRef } from 'react';
+import useVisitor from '../../../hooks/useVisitor';
+import { useMoreSheetStore } from '../../../store/useMoreSheetStore';
+import MyPostModal from './MyPostModal';
+import OthersPostModal from './OthersPostModal';
+
+const MoreBottomSheet = () => {
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+  const { data } = useVisitor();
+  const { selectedPostId, authorId, isMoreSheetVisible, hideMoreSheet } = useMoreSheetStore();
+
+  const isMine = data?.userId === authorId;
+
+  useEffect(() => {
+    if (isMoreSheetVisible) {
+      bottomSheetRef.current?.present();
+    } else {
+      bottomSheetRef.current?.dismiss();
+    }
+  }, [isMoreSheetVisible]);
+
+  return (
+    <CustomBottomSheet
+      ref={bottomSheetRef}
+      backgroundColor="transparent"
+      onChange={(index) => {
+        if (index === -1) {
+          hideMoreSheet();
+        }
+      }}
+    >
+      {selectedPostId && authorId ? (
+        isMine ? (
+          <MyPostModal closeModal={hideMoreSheet} postId={selectedPostId} />
+        ) : (
+          <OthersPostModal closeModal={hideMoreSheet} postId={selectedPostId} authorId={authorId} />
+        )
+      ) : (
+        <></>
+      )}
+    </CustomBottomSheet>
+  );
+};
+
+export default MoreBottomSheet;
