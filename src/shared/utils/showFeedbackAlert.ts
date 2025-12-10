4a0fad191e5f4a6433cff4d813ec5ddb9e0c4af5
shinhm1 <shinhm1@naver.com>
2025-12-10 18:02:21 +0900

    feat: 피드백 alert 호출 유틸 함수 구현

---
+++ src/shared/utils/showFeedbackAlert.ts
@@ -0,0 +1,31 @@
+import { Alert } from 'react-native';
+
+export async function showFeedbackAlert(onPressHandler: (satisfied: string) => Promise<void>) {
+  Alert.alert(
+    'How was your experience with Kori?',
+    "We'd love to hear your valuable feedback. Your input will help us provide a better service.",
+    [
+      {
+        text: 'Not really',
+        onPress: async () => onPressHandler('Not Satisfied'),
+      },
+      {
+        text: 'Good',
+        onPress: async () => onPressHandler('Satisfied'),
+      },
+    ],
+  );
+  return null;
+}
+
+export function showFeedbackFailedAlert() {
+  Alert.alert('Feedback Submit Failed', 'There was an issue sending your feedback. Please try again later.', [
+    { text: 'Close' },
+  ]);
+}
+
+export function showFeedbackDoneAlert() {
+  Alert.alert('Thank you for your feedback!', 'We appreciate your time and effort in helping us improve Kori.', [
+    { text: 'Close' },
+  ]);
+}
