28d10d582b138f20a3534be47027e1f7b48c0a32
deokjin <ghaehfdl7@naver.com>
2025-12-16 09:35:32 +0900

    refactor: 사용자 프로필 조회 및 팔로우 관련 src/shared로 이관

---
+++ src/shared/api/userProfile.ts
@@ -0,0 +1,20 @@
+import api from '@/api/axiosInstance';
+import { User } from '../types/user';
+
+/** 사용자 프로필 조회 */
+export const fetchUserProfile = async (userId: number): Promise<User> => {
+  const response = await api.get(`/api/v1/member/${userId}/info`);
+  return response.data;
+};
+
+/** 사용자 팔로우 */
+export const followUser = async (userId: number) => {
+  const response = await api.post(`/api/v1/home/follow/${userId}`);
+  return response.data;
+};
+
+/** 사용자 언팔로우 */
+export const unfollowUser = async (userId: number) => {
+  const response = await api.delete(`/api/v1/home/follow/${userId}`);
+  return response.data;
+};
