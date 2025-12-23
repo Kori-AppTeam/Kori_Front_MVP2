7210b51515ce11f8d2e57e414212d6cafe3b596e
JOEIH <joh73459@gmail.com>
2025-12-24 03:27:21 +0900

    refactor: 마이히스토리 게시글 카드 생성

--- src/features/community/post/components/MyHistoryCard.tsx
+++ src/features/community/post/components/MyHistoryCard.tsx
@@ -1,10 +1,45 @@
+import { COMMUNITY_ROUTER } from '@/src/shared/constants/route';
+import { router } from 'expo-router';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { SCREEN_WIDTH } from '../../shared/constants/constants';
+import { BorderLine, Container, ContentBox, Wrap } from '../../shared/styles/styles';
+import { useHandleLikeBookmark } from '../hooks/useHandleLikeBookmark';
+import { useMoreSheetStore } from '../store/useMoreSheetStore';
+import { MyHistoryPost } from '../types';
+import PostTextContent from './elements/body/PostTextContent';
+import PostCommonFooter from './elements/footer/PostCommonFooter';
+import PostCommonHeader from './elements/header/PostCommonHeader';
 
-const MyHistoryCard = () => {
-  return <View></View>;
+type MyHistoryCardProps = {
+  data: MyHistoryPost;
+  authorId?: number;
 };
 
-const styles = StyleSheet.create({});
+const MyHistoryCard = ({ authorId, data }: MyHistoryCardProps) => {
+  const { showMoreSheet } = useMoreSheetStore();
+  const { handleToggleLike } = useHandleLikeBookmark();
+
+  return (
+    <Container>
+      <Wrap width={SCREEN_WIDTH} onPress={() => router.push(COMMUNITY_ROUTER.DETAIL(data.postId))}>
+        <PostCommonHeader postId={data.postId} createdAt={data.createdAt} viewCount={data.viewCount} />
+
+        <ContentBox>
+          <PostTextContent isTruncate={true} postId={data.postId} content={data.content} />
+        </ContentBox>
+
+        <PostCommonFooter
+          commentCount={data.commentCount}
+          isLiked={data.isLiked}
+          likeCount={data.likeCount}
+          onToggleLike={() => handleToggleLike(data.postId, data.isLiked)}
+          onToggleComment={() => router.push(COMMUNITY_ROUTER.DETAIL(data.postId))}
+          onOpenModal={() => authorId && showMoreSheet('post', data.postId, authorId)}
+        />
+      </Wrap>
+      <BorderLine width={SCREEN_WIDTH} />
+    </Container>
+  );
+};
 
 export default MyHistoryCard;
