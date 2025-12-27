c10d0f3687ce847325aa7cacdd4dc50c9e13de5d
JOEIH <joh73459@gmail.com>
2025-12-27 23:37:32 +0900

    refactor: 댓글 작성 시 불필요한 refetch 일어나지 않도록 하기 위해 쿼리 무효화 범위 지정

--- hooks/mutations/useCreateComment.ts
+++ hooks/mutations/useCreateComment.ts
@@ -13,8 +13,8 @@ export function useCreateComment(postId: number) {
     },
     onSuccess: () => {
       if (postId) {
-        qc.invalidateQueries({ queryKey: ['post'] });
-        qc.invalidateQueries({ queryKey: ['comments'] });
+        qc.invalidateQueries({ queryKey: ['post', postId] });
+        qc.invalidateQueries({ queryKey: ['comments', 'post', postId] });
       }
     },
   });
