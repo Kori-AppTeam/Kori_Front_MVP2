d63d63b8070fe4d9b10118592219837dad7f1424
JOEIH <joh73459@gmail.com>
2026-01-03 21:03:33 +0900

    chore: 백엔드 변경에 따라 타입 수정

--- src/features/k-culture/constants/categoryMapper.ts
+++ src/features/k-culture/constants/categoryMapper.ts
@@ -1,19 +1,11 @@
 import { NewsType } from '../types';
 
 export const NEWS_CATEGORY_MAPPER = {
-  K_POP: '🎧K-POP',
-  K_DRAMA: '💖K-Drama',
-  K_BEAUTY: '💄K-Beauty',
-  K_FASHION: '🧢K-Fashion',
-  K_CULTURE: '🇰🇷K-Culture',
+  'K-POP': '🎧K-POP',
+  'K-DRAMA': '💖K-Drama',
+  'K-BEAUTY': '💄K-Beauty',
+  'K-FASHION': '🧢K-Fashion',
+  'K-CULTURE': '🇰🇷K-Culture',
 } as const;
 
 export const NEWS_CATEGORIES = Object.keys(NEWS_CATEGORY_MAPPER) as NewsType[];
-
-export const NEW_CATEGORY_MAPPER_NO_EMOJI = {
-  K_POP: 'K-POP',
-  K_DRAMA: 'K-Drama',
-  K_BEAUTY: 'K-Beauty',
-  K_FASHION: 'K-Fashion',
-  K_CULTURE: 'K-Culture',
-} as const;
