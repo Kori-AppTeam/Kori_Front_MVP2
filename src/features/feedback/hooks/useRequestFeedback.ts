c555bbcc8c7b988d7480dc7dbd3e418704b2e7e5
shinhm1 <shinhm1@naver.com>
2026-01-03 20:28:22 +0900

    chore: 테스트용 코드 주석 추가

--- src/features/feedback/hooks/useRequestFeedback.ts
+++ src/features/feedback/hooks/useRequestFeedback.ts
@@ -41,6 +41,11 @@ export function useRequestFeedback() {
     });
 
     await AsyncStorage.setItem('START_COUNT', String(startCount + 1));
+
+    /* NOTE 테스트용 코드, 테스트 시 주석을 풀고 실행해 주세요 */
+    // await AsyncStorage.setItem('START_COUNT', '2');
+    // await AsyncStorage.setItem('HAS_REQUESTED_FEEDBACK', 'false');
+
     // 이전에 피드백을 요청한 적이 있는지 확인
     const hasRequested = await AsyncStorage.getItem('HAS_REQUESTED_FEEDBACK').then((value) => {
       return value === 'true';
