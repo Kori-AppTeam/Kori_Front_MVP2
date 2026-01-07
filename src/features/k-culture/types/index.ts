e177aa696053af17be81bcc3e206fe2f75c6e716
JOEIH <joh73459@gmail.com>
2026-01-08 01:36:07 +0900

    chore: 퀴즈, 투표 타입 추가

--- src/features/k-culture/types/index.ts
+++ src/features/k-culture/types/index.ts
@@ -63,3 +63,21 @@ export interface NewsPageParams {
   size: number;
   cursor?: string;
 }
+
+// 퀴즈, 투표
+export interface QuizAndVoteItem {
+  id: number;
+  type: 'QUIZ' | 'VOTE';
+  title: string;
+  description: string;
+  closeAt: string;
+  totalVoteCount: number;
+  options: OptionType[];
+  selectedOptionId: number | null;
+}
+
+export interface OptionType {
+  id: number;
+  content: string;
+  voteCount: number;
+}
