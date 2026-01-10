4f2550e93025ba989b375e09ffeb24ea0a57d472
shinhm1 <shinhm1@naver.com>
2026-01-10 19:18:07 +0900

    feat: 공통 컴포넌트 Dropdown Input 구현

---
+++ src/shared/components/DropdownInput.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Icon from '@/components/common/Icon';
+import { textStyle, theme } from '@/src/styles/theme';
+import styled from 'styled-components/native';
+
+interface DropdownInputProps {
+  label: string;
+  value?: string;
+  selectedCount?: number;
+  maxCount?: number;
+  isOpen: boolean;
+  onPress: () => void;
+}
+
+const DropdownInput = ({ label, value, selectedCount, maxCount, isOpen, onPress }: DropdownInputProps) => {
+  return (
+    <>
+      <DropdownInputContainer isOpen={isOpen} onPress={onPress}>
+        <DropdownInputPlaceholder>{value || label}</DropdownInputPlaceholder>
+        <Icon
+          type={isOpen ? 'dropdownArrowUp' : 'dropdownArrowDown'}
+          size={32}
+          color={isOpen ? theme.colors.primary.mint : theme.colors.primary.white}
+        />
+      </DropdownInputContainer>
+      {maxCount && (
+        <DropdownSelectedCount hasValue={!!value}>{`${selectedCount} / ${maxCount} selected`}</DropdownSelectedCount>
+      )}
+    </>
+  );
+};
+
+export default DropdownInput;
+
+const DropdownInputContainer = styled.Pressable<{ isOpen: boolean }>`
+  width: 100%;
+  height: 48px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 12px 0 16px;
+  background-color: ${({ theme }) => theme.colors.gray.darkGray_1};
+  border: 1px solid ${({ theme, isOpen }) => (isOpen ? theme.colors.primary.mint : theme.colors.gray.darkGray_2)};
+  border-radius: 4px;
+`;
+
+const DropdownInputPlaceholder = styled.Text`
+  ${({ theme }) => textStyle(theme.fonts.body.B2_R)};
+  color: ${({ theme }) => theme.colors.primary.white};
+`;
+
+const DropdownSelectedCount = styled.Text<{ hasValue: boolean }>`
+  flex: 1;
+  width: 100%;
+  text-align: right;
+  color: ${({ theme, hasValue }) => (hasValue ? theme.colors.primary.mint : theme.colors.gray.gray_1)};
+  ${({ theme }) => textStyle(theme.fonts.body.B5_M)};
+`;
