0e420a67ff94ed1d0dd20e332ea779c8a29592f4
deokjin <ghaehfdl7@naver.com>
2026-01-15 22:47:59 +0900

    fix: 팔로우 수락 및 거절 기능 추가와 캐시 무효화 로직 수정

--- src/shared/hooks/useFollowQuery.ts
+++ src/shared/hooks/useFollowQuery.ts
@@ -1,4 +1,10 @@
-import { cancelFollowUser, followUser, unfollowUser } from '@/src/shared/api/follow';
+import {
+  acceptFollowUser,
+  cancelFollowUser,
+  declineFollowUser,
+  followUser,
+  unfollowUser,
+} from '@/src/shared/api/follow';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { USER_PROFILE_QK } from './useUserProfileQuery';
 
@@ -43,6 +49,41 @@ export const useCancelFollowUserMutation = () => {
       qc.invalidateQueries({ queryKey: USER_PROFILE_QK(userId) });
       // 팔로우 요청 목록 캐시 무효화
       qc.invalidateQueries({ queryKey: ['follow-list', 'PENDING', 'sent'] });
+    },
+  });
+};
+
+/** 팔로우 수락 Mutation */
+export const useAcceptFollowUserMutation = () => {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: (userId: number) => acceptFollowUser(userId),
+    onSuccess: async (_, userId) => {
+      // 해당 사용자 프로필 캐시 무효화
+      // 팔로우 요청 목록 캐시 무효화, 친구 목록 캐시 무효화
+      await Promise.all([
+        qc.invalidateQueries({ queryKey: USER_PROFILE_QK(userId) }),
+        qc.invalidateQueries({ queryKey: ['follow-list', 'PENDING', 'received'] }),
+        qc.invalidateQueries({ queryKey: ['following', 'ACCEPTED'] }),
+      ]);
+    },
+  });
+};
+
+/** 팔로우 거절 Mutation */
+export const useDeclineFollowUserMutation = () => {
+  const qc = useQueryClient();
+
+  return useMutation({
+    mutationFn: (userId: number) => declineFollowUser(userId),
+    onSuccess: async (_, userId) => {
+      // 해당 사용자 프로필 캐시 무효화
+      // 팔로우 요청 목록 캐시 무효화
+      await Promise.all([
+        qc.invalidateQueries({ queryKey: USER_PROFILE_QK(userId) }),
+        qc.invalidateQueries({ queryKey: ['follow-list', 'PENDING', 'received'] }),
+      ]);
     },
   });
 };
