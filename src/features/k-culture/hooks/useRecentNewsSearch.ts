4ca147b7f24cb26d3d7555dc61724866840a0019
JOEIH <joh73459@gmail.com>
2026-01-16 21:52:37 +0900

    feat: 뉴스 최근 검색어 훅 생성

---
+++ src/features/k-culture/hooks/useRecentNewsSearch.ts
@@ -0,0 +1,35 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { deleteAllRecentNewsSearches, deleteRecentNewsSearch, getRecentNewsSearches } from '../apis/search';
+
+// 뉴스 최근 검색어 조회
+export const useGetRecentNewsSearch = () => {
+  return useQuery<string[]>({
+    queryKey: ['recentNewsSearches'],
+    queryFn: () => getRecentNewsSearches(),
+    staleTime: 0,
+  });
+};
+
+// 뉴스 최근 검색어 단건 삭제
+export const useDeleteRecentNewsSearch = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: (q: string) => deleteRecentNewsSearch(q),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['recentNewsSearches'] });
+    },
+  });
+};
+
+// 뉴스 최근 검색어 전체 삭제
+export const useClearRecentNewsSearches = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: () => deleteAllRecentNewsSearches(),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['recentNewsSearches'] });
+    },
+  });
+};
