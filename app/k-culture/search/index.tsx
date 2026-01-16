53e3a4890b71835c092cc72ce1b674c44d19e84b
JOEIH <joh73459@gmail.com>
2026-01-16 21:56:05 +0900

    feat: 뉴스 검색 페이지 추천 검색어 제외한 기능들 적용

--- app/k-culture/search/index.tsx
+++ app/k-culture/search/index.tsx
@@ -1,10 +1,89 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { useDebounce } from '@/src/features/chat/search/hooks/useDebounce';
+import KNewsAutoComplete from '@/src/features/k-culture/components/KNewsAutoComplete';
+import SearchedNewsResult from '@/src/features/k-culture/components/SearchedNewsResult';
+import {
+  useClearRecentNewsSearches,
+  useDeleteRecentNewsSearch,
+  useGetRecentNewsSearch,
+} from '@/src/features/k-culture/hooks/useRecentNewsSearch';
+import RecentSearches from '@/src/shared/components/RecentSearches';
+import SearchInput from '@/src/shared/components/SearchInput';
+import React, { useState } from 'react';
+import { Keyboard, TouchableWithoutFeedback } from 'react-native';
+import styled from 'styled-components/native';
 
 const Index = () => {
-  return <View>추가될 예정입니다.</View>;
+  const [value, setValue] = useState<string>('');
+  const [isSubmitted, setIsSubmitted] = useState<boolean>(false);
+
+  const { data: recentNewsSearches } = useGetRecentNewsSearch();
+  const { mutate: deleteRecentNewsSearch } = useDeleteRecentNewsSearch();
+  const { mutate: clearRecentNewsSearches } = useClearRecentNewsSearches();
+  const debouncedValue = useDebounce(value);
+  const effectiveQ = debouncedValue.trim().toLowerCase();
+
+  // 검색어 변경 처리
+  const handleChangeText = (text: string) => {
+    setValue(text);
+    setIsSubmitted(false);
+  };
+
+  // 검색어 제출 처리
+  const handleSubmit = () => {
+    if (value.trim().length === 0) return;
+    setIsSubmitted(true);
+    Keyboard.dismiss();
+  };
+
+  // 최근 검색어, 자동완성에서 검색어 제출 처리
+  const handleSubmitAutoComplete = (text: string) => {
+    if (text.length === 0) return;
+    setValue(text);
+    setIsSubmitted(true);
+    Keyboard.dismiss();
+  };
+
+  return (
+    <Safe>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <SearchInput
+            value={value}
+            onChangeText={handleChangeText}
+            placeholder="Search Anything"
+            onSubmitEditing={handleSubmit}
+          />
+
+          {/* 최근검색어 */}
+          {!value && recentNewsSearches && recentNewsSearches.length > 0 && !isSubmitted && (
+            <RecentSearches
+              data={recentNewsSearches}
+              onPressKeyword={handleSubmitAutoComplete}
+              onDeleteKeyword={deleteRecentNewsSearch}
+              onClearAll={clearRecentNewsSearches}
+            />
+          )}
+
+          {/* 자동완성 */}
+          {value.trim().length > 0 && !isSubmitted && (
+            <KNewsAutoComplete value={effectiveQ} onSubmitEditing={handleSubmitAutoComplete} />
+          )}
+
+          {/* 검색 결과 */}
+          {value.trim().length > 0 && isSubmitted && <SearchedNewsResult value={value.trim().toLowerCase()} />}
+        </Container>
+      </TouchableWithoutFeedback>
+    </Safe>
+  );
 };
 
-const styles = StyleSheet.create({});
-
 export default Index;
+
+const Container = styled.View`
+  flex: 1;
+`;
+
+const Safe = styled.SafeAreaView`
+  flex: 1;
+  background: #1d1e1f;
+`;
