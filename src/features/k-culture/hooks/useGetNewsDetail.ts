c0902325629425a4c2dc2d229bee14ef8ab71985
JOEIH <joh73459@gmail.com>
2026-01-16 21:57:32 +0900

    feat: 뉴스 상세조회 훅에서 뉴스 검색 경로일 경우 다른 api 적용 가능하도록 수정

--- src/features/k-culture/hooks/useGetNewsDetail.ts
+++ src/features/k-culture/hooks/useGetNewsDetail.ts
@@ -1,11 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { getNewsDetail } from '../apis/news';
+import { getNewsDetailForSearch } from '../apis/search';
 import { KCultureNewsItem } from '../types';
 
-export const useGetNewsDetail = (contentId: number) => {
+export const useGetNewsDetail = (contentId: number, fromSearch: boolean) => {
   return useQuery<KCultureNewsItem>({
-    queryKey: ['news', 'detail', contentId],
-    queryFn: () => getNewsDetail(contentId),
+    queryKey: ['news', 'detail', fromSearch ? 'search' : 'normal', contentId],
+    queryFn: () => {
+      return fromSearch ? getNewsDetailForSearch(contentId) : getNewsDetail(contentId);
+    },
     enabled: !!contentId,
   });
 };
