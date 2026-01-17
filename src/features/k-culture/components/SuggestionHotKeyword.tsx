bc1a993d7ab2cfb3b2d5f32fae663a2eb1b39e30
JOEIH <joh73459@gmail.com>
2026-01-17 16:02:04 +0900

    feat: 뉴스 추천 검색어 컴포넌트 생성

---
+++ src/features/k-culture/components/SuggestionHotKeyword.tsx
@@ -0,0 +1,48 @@
+import HotKeywordChip from '@/src/shared/components/HotKeywordChip';
+import RefreshButton from '@/src/shared/components/RefreshButton';
+import { textStyle, theme } from '@/src/styles/theme';
+import React from 'react';
+import styled from 'styled-components/native';
+
+type SuggestionHotKeywordProps = {
+  data: string[];
+  onPress?: (keyword: string) => void;
+  onRefresh?: () => void;
+};
+
+const SuggestionHotKeyword = ({ data, onPress, onRefresh }: SuggestionHotKeywordProps) => {
+  return (
+    <Container>
+      <Title>Search Suggestions</Title>
+
+      <KeywordsContainer>
+        {data.map((keyword, index) => (
+          <HotKeywordChip key={index} value={keyword} onPress={() => onPress?.(keyword)} />
+        ))}
+        <RefreshButton onPress={onRefresh} />
+      </KeywordsContainer>
+    </Container>
+  );
+};
+
+export default SuggestionHotKeyword;
+
+const Container = styled.View`
+  flex: 1;
+  padding: 10px 20px;
+  gap: 24px;
+`;
+
+const Title = styled.Text`
+  width: 100%;
+  text-align: left;
+  color: ${theme.colors.gray.gray_1};
+  ${({ theme }) => textStyle(theme.fonts.body.B5_SB)};
+`;
+
+const KeywordsContainer = styled.View`
+  width: 100%;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 8px;
+`;
