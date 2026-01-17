c15b035a269477f8915f87fc6bfd8d6d86c4a37a
JOEIH <joh73459@gmail.com>
2026-01-17 19:08:14 +0900

    style: 추천 검색어 컴포넌트 패딩값 수정

--- src/features/k-culture/components/SuggestionHotKeyword.tsx
+++ src/features/k-culture/components/SuggestionHotKeyword.tsx
@@ -28,7 +28,7 @@ const SuggestionHotKeyword = ({ data, onPress, onRefresh }: SuggestionHotKeyword
 export default SuggestionHotKeyword;
 
 const Container = styled.View`
-  padding: 24px 20px;
+  padding: 10px 20px;
   gap: 24px;
 `;
 
