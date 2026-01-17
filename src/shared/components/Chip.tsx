37f67da5530c8241bbcf8f6b646bb0867406cf27
JOEIH <joh73459@gmail.com>
2026-01-17 21:40:42 +0900

    style: 공용 텍스트칩 배경색, 텍스트색 지정할 수 있도록 수정

--- src/shared/components/Chip.tsx
+++ src/shared/components/Chip.tsx
@@ -5,20 +5,22 @@ import styled from 'styled-components/native';
 type ChipProps = {
   value: string;
   onPress?: (keyword: string) => void;
+  bgColor?: string;
+  textColor?: string;
 };
 
-const Chip = ({ value, onPress }: ChipProps) => {
+const Chip = ({ value, onPress, bgColor, textColor }: ChipProps) => {
   return (
-    <ChipContainer onPress={() => onPress && onPress(value)}>
-      <ChipText>{value}</ChipText>
+    <ChipContainer onPress={() => onPress && onPress(value)} bgColor={bgColor}>
+      <ChipText textColor={textColor}>{value}</ChipText>
     </ChipContainer>
   );
 };
 
 export default Chip;
 
-const ChipContainer = styled.Pressable`
-  background-color: ${theme.colors.gray.darkGray_1_5};
+const ChipContainer = styled.Pressable<{ bgColor?: string }>`
+  background-color: ${(props) => props.bgColor || theme.colors.gray.darkGray_1_5};
   padding: 10px 12px;
   border-radius: 100px;
   align-items: center;
@@ -27,7 +29,7 @@ const ChipContainer = styled.Pressable`
   align-self: flex-start;
 `;
 
-const ChipText = styled.Text`
+const ChipText = styled.Text<{ textColor?: string }>`
   ${({ theme }) => textStyle(theme.fonts.body.B4_R)};
-  color: ${theme.colors.primary.white};
+  color: ${(props) => props.textColor || theme.colors.primary.white};
 `;
