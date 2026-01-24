7fc1556c7c95f6ccd77261f6b54ba89f8f690704
JOEIH <joh73459@gmail.com>
2026-01-24 19:35:05 +0900

    chore: 퀴즈, 투표 api 폴더 이동

--- src/features/k-culture/apis/quizAndVote.ts
+++
@@ -1,8 +0,0 @@
-import api from '@/api/axiosInstance';
-
-export const getTodayQuizAndVote = async (type: 'QUIZ' | 'VOTE') => {
-  const res = await api.get(`/api/v1/today`, {
-    params: { type },
-  });
-  return res.data;
-};
