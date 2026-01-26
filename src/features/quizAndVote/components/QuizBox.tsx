46f453e41e55284dee155d85ad6a748664b6c73d
JOEIH <joh73459@gmail.com>
2026-01-27 01:55:25 +0900

    feat: 퀴즈 참여 후 결과가 반영되도록 수정

--- src/features/quizAndVote/components/QuizBox.tsx
+++ src/features/quizAndVote/components/QuizBox.tsx
@@ -9,9 +9,11 @@ import QuizOption from './QuizOption';
 
 const QuizBox = ({ data }: { data: TodayPollType }) => {
   const [isSelectedOption, setIsSelectedOption] = useState<number | null>(data.selectedOptionId ?? null);
-  const [showResult, setShowResult] = useState(data.selectedOptionId ? true : false);
+  const [showResult, setShowResult] = useState(!!data.selectedOptionId); // 이미 선택한 옵션이 있으면 결과 보여주기
 
   const { mutate: postPoll, data: pollResult } = usePostPoll();
+
+  const answerOptionId = pollResult?.correctOptionId ?? data.correctOptionId;
 
   const handleSelectOption = (optionId: number) => {
     if (showResult) return; // 결과가 보여지는 상태에서는 선택 불가(퀴즈 참여 한 번만 가능)
@@ -44,7 +46,7 @@ const QuizBox = ({ data }: { data: TodayPollType }) => {
             isSelected={isSelectedOption === option.id}
             isResult={showResult}
             onPress={() => handleSelectOption(option.id)}
-            correctOptionId={showResult ? data.correctOptionId : null}
+            correctOptionId={showResult ? answerOptionId : null}
           />
         ))}
       </OptionRow>
