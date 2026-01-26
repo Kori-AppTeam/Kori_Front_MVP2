a3b43a44c2073217cfdc3990bfb6af864e2179f5
shinhm1 <shinhm1@naver.com>
2026-01-27 00:32:56 +0900

    chore: api 에러 alert 보강

--- src/features/auth/api/postEmailCheck.ts
+++ src/features/auth/api/postEmailCheck.ts
@@ -1,13 +1,19 @@
 import { Config } from '@/src/shared/constants/config';
-import axios from 'axios';
+import axios, { isAxiosError } from 'axios';
+import { Alert } from 'react-native';
 
 // 이미 가입된 이메일이 존재하는지 확인
 export async function postEmailCheck(email: string) {
   try {
+    Alert.alert('Debug', `API URL: ${Config.SERVER_URL}/api/v1/member/email/check`);
     const response = await axios.post(`${Config.SERVER_URL}/api/v1/member/email/check`, { email: email });
     const { exists } = response.data.data;
     return exists;
   } catch (error) {
+    if (isAxiosError(error)) {
+      Alert.alert('API ERROR', `${error.response?.data?.message || error.message}`);
+    }
+
     throw error;
   }
 }
