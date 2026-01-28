29489faabea8d47da9a1066a52467d9287019c1d
JOEIH <joh73459@gmail.com>
2026-01-28 22:16:13 +0900

    feat: post의 이미지, 퀴즈, 투표 처리하는 공통 media 컴포넌트 생성

---
+++ src/features/community/post/components/elements/body/PostCommonMedia.tsx
@@ -0,0 +1,56 @@
+import { SCREEN_WIDTH } from '@/src/features/community/shared/constants/constants';
+import QuizBox from '@/src/features/quizAndVote/components/QuizBox';
+import VoteBox from '@/src/features/quizAndVote/components/VoteBox';
+import { PollBaseType } from '@/src/features/quizAndVote/types';
+import React from 'react';
+import { hasValidImages } from '../../../utils/postUtils';
+import PostCarousel from './PostCarousel';
+import PostSingleImage from './PostSingleImage';
+
+interface PostCommonMediaProps {
+  type: 'GENERAL' | 'QUIZ' | 'VOTE';
+  images?: string[] | string | null;
+  imageCount?: number;
+  pollInfo?: PollBaseType;
+  pollId?: number;
+}
+
+const PostCommonMedia = ({ type, images, imageCount, pollInfo, pollId }: PostCommonMediaProps) => {
+  if (pollId && pollInfo) {
+    return (
+      <>
+        {type === 'QUIZ' && <QuizBox pollId={pollId} data={pollInfo} />}
+        {type === 'VOTE' && <VoteBox pollId={pollId} data={pollInfo} />}
+      </>
+    );
+  }
+
+  // 이미지 유효성 검사: 없거나 빈 값일 경우 null 반환
+  if (!hasValidImages(images)) {
+    return null;
+  }
+
+  const imageList = Array.isArray(images) ? images : [images];
+
+  return (
+    <>
+      {imageList.length > 1 ? (
+        <PostCarousel
+          images={imageList as string[]}
+          gap={5}
+          offset={20}
+          pageWidth={SCREEN_WIDTH}
+          imageCount={imageCount ?? imageList.length}
+        />
+      ) : (
+        <PostSingleImage
+          imageUrl={imageList[0] as string}
+          imageCount={imageCount ?? imageList.length}
+          pageWidth={SCREEN_WIDTH - 20 * 2}
+        />
+      )}
+    </>
+  );
+};
+
+export default PostCommonMedia;
