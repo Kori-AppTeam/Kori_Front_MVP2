effdc226f9dd7bbc660c3664f1cee89b89926d39
JOEIH <joh73459@gmail.com>
2026-01-28 22:13:42 +0900

    chore: 서버 게시글 타입 변경에 따른 타입 정의 수정

--- src/features/community/post/types/index.ts
+++ src/features/community/post/types/index.ts
@@ -19,7 +19,7 @@ export type AllowedClientCategory =
 
 // 게시글 상세
 export interface PostDetail {
-  postId: number;
+  id: number;
   content: string;
   authorId: number;
   authorName: string | null;
@@ -34,12 +34,29 @@ export interface PostDetail {
   viewCount: number;
   userImageUrl: string;
   contentImageUrls?: string[];
-  imageCount: number;
 }
+
+// 일반 게시글 상세
+export interface GeneralPostDetail extends PostDetail {
+  category: GeneralCategory;
+  postInfo: {
+    contentImageUrl: string[] | null;
+    imageCount: number;
+  };
+}
+
+// 투표/퀴즈 게시글 상세
+export interface PostPollDetail extends PostDetail {
+  category: PollCategory;
+  pollInfo: PollBaseType;
+}
+
+// 게시글 상세 타입 (일반 게시글 + 투표/퀴즈 게시글)
+export type PostDetailType = GeneralPostDetail | PostPollDetail;
 
 // 게시글 전체목록 조회 시 게시글
 export interface PostsListItem {
-  postId: number;
+  id: number;
   contentPreview: string;
   authorId: number;
   authorName: string | null;
@@ -70,6 +87,7 @@ export interface PostPollItem extends PostsListItem {
   pollInfo: PollBaseType;
 }
 
+// 게시글 리스트 아이템 타입 (일반 게시글 + 투표/퀴즈 게시글)
 export type PostsListItemType = GeneralPostItem | PostPollItem;
 
 // page별 post 호출
@@ -106,7 +124,7 @@ export interface RequestPageParams {
 // 북마크 게시글 아이템
 export interface BookmarkedPostItem {
   bookmarkId: number;
-  postId: number;
+  id: number;
   authorId: number;
   authorName: string;
   content: string;
@@ -210,7 +228,7 @@ export interface CreateCommentReq {
 
 // 마이 히스토리 게시글 타입
 export interface MyHistoryPost {
-  postId: number;
+  id: number;
   content: string;
   createdAt: string;
   isLiked: boolean;
