7935e7f5caf662a55d645ac792184a82db23eec5
JOEIH <joh73459@gmail.com>
2026-01-29 23:03:29 +0900

    feat: 투표 글 작성 api 추가

--- src/features/community/write/apis/write.ts
+++ src/features/community/write/apis/write.ts
@@ -1,5 +1,5 @@
 import api from '@/api/axiosInstance';
-import { CreatePostBody, UpdatePostBody } from '../types';
+import { CreatePostBody, CreateVotePostBody, UpdatePostBody } from '../types';
 
 // 게시글 작성
 export async function createPost(boardId: number, body: CreatePostBody) {
@@ -11,4 +11,10 @@ export async function createPost(boardId: number, body: CreatePostBody) {
 export async function updatePost(postId: number, body: UpdatePostBody) {
   await api.put(`/api/v1/posts/${postId}`, body);
   return true;
+}
+
+// 투표 게시글 작성
+export async function createVotePost(body: CreateVotePostBody) {
+  const { data } = await api.post(`/api/v2/poll/vote`, body);
+  return data as string;
 }
