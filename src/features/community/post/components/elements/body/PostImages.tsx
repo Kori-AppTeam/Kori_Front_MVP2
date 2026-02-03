2c6e98b144936c633b2a8989153749ad5a261333
JOEIH <joh73459@gmail.com>
2026-02-03 18:47:07 +0900

    refactor: 텍스트/미디어 배치 상이로 인한 PostCommonMedia 컴포넌트 분리

--- src/features/community/post/components/elements/body/PostImages.tsx
+++ src/features/community/post/components/elements/body/PostImages.tsx
@@ -1,33 +1,15 @@
 import { SCREEN_WIDTH } from '@/src/features/community/shared/constants/constants';
-import QuizBox from '@/src/features/quizAndVote/components/QuizBox';
-import VoteBox from '@/src/features/quizAndVote/components/VoteBox';
-import { PollBaseType } from '@/src/features/quizAndVote/types';
-import { QueryKey } from '@tanstack/react-query';
 import React from 'react';
-import styled from 'styled-components/native';
 import { hasValidImages } from '../../../utils/postUtils';
 import PostCarousel from './PostCarousel';
 import PostSingleImage from './PostSingleImage';
 
-interface PostCommonMediaProps {
-  type: 'GENERAL' | 'QUIZ' | 'VOTE';
+interface PostImagesProps {
   images?: string[] | string | null;
   imageCount?: number;
-  pollInfo?: PollBaseType;
-  pollId?: number;
-  queryKey?: QueryKey;
 }
 
-const PostCommonMedia = ({ type, images, imageCount, pollInfo, pollId, queryKey }: PostCommonMediaProps) => {
-  if (pollId && pollInfo) {
-    return (
-      <PollContainer>
-        {type === 'QUIZ' && <QuizBox pollId={pollId} data={pollInfo} queryKey={queryKey} />}
-        {type === 'VOTE' && <VoteBox pollId={pollId} data={pollInfo} queryKey={queryKey} />}
-      </PollContainer>
-    );
-  }
-
+const PostImages = ({ images, imageCount }: PostImagesProps) => {
   // 이미지 유효성 검사: 없거나 빈 값일 경우 null 반환
   if (!hasValidImages(images)) {
     return null;
@@ -56,9 +38,4 @@ const PostCommonMedia = ({ type, images, imageCount, pollInfo, pollId, queryKey 
   );
 };
 
-export default PostCommonMedia;
-
-const PollContainer = styled.View`
-  width: 100%;
-  padding: 0 20px;
-`;
+export default PostImages;
