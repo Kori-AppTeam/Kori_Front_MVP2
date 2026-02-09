d99cb333cd334596fe9c1d04ec2be58955ae0161
JOEIH <joh73459@gmail.com>
2026-02-10 01:27:24 +0900

    refactor: 사용하지 않는 queryKey prop 제거

--- src/features/community/post/components/elements/body/PostPoll.tsx
+++ src/features/community/post/components/elements/body/PostPoll.tsx
@@ -1,7 +1,6 @@
 import QuizBox from '@/src/features/quizAndVote/components/QuizBox';
 import VoteBox from '@/src/features/quizAndVote/components/VoteBox';
 import { PollBaseType } from '@/src/features/quizAndVote/types';
-import { QueryKey } from '@tanstack/react-query';
 import React from 'react';
 import styled from 'styled-components/native';
 
@@ -9,18 +8,17 @@ interface PostPollProps {
   type: 'QUIZ' | 'VOTE';
   pollInfo?: PollBaseType;
   pollId?: number;
-  queryKey?: QueryKey;
 }
 
-const PostPoll = ({ type, pollInfo, pollId, queryKey }: PostPollProps) => {
+const PostPoll = ({ type, pollInfo, pollId }: PostPollProps) => {
   if (!pollId || !pollInfo) {
     return null;
   }
 
   return (
     <PollContainer>
-      {type === 'QUIZ' && <QuizBox pollId={pollId} data={pollInfo} queryKey={queryKey} />}
-      {type === 'VOTE' && <VoteBox pollId={pollId} data={pollInfo} queryKey={queryKey} />}
+      {type === 'QUIZ' && <QuizBox pollId={pollId} data={pollInfo} />}
+      {type === 'VOTE' && <VoteBox pollId={pollId} data={pollInfo} />}
     </PollContainer>
   );
 };
