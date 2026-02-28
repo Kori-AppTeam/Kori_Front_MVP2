bf0728c9e3c83aa4f197e6bd1e22ff04965e21a2
JOEIH <joh73459@gmail.com>
2026-02-28 16:52:10 +0900

    feat: 유저 채팅 목록, 게시글 목록 조회 api 추가

--- src/features/profile/apis/profile.ts
+++ src/features/profile/apis/profile.ts
@@ -1,1 +1,13 @@
-// 프로필 관련 api 모음
+import api from '@/api/axiosInstance';
+
+// 해당 유저가 참여한 채팅방 리스트 조회 API
+export const getUserLinkedSpaces = async (userId: number) => {
+  const response = await api.get(`/api/v1/member/profile/${userId}/chat-rooms`);
+  return response.data.data;
+};
+
+// 해당 유저의 게시글 리스트 조회 API
+export const getUserPosts = async (userId: number) => {
+  const response = await api.get(`/api/v1/member/profile/${userId}/posts`);
+  return response.data.data;
+};
