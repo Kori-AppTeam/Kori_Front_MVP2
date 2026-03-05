be65124f03ed7944921a7f0750a0185c90ca5e1d
JOEIH <joh73459@gmail.com>
2026-03-06 00:53:18 +0900

    refactor: 친구 관련 버튼 로직 재사용 가능하도록 수정

--- src/shared/hooks/useFriendAction.ts
+++ src/shared/hooks/useFriendAction.ts
@@ -7,34 +7,39 @@ import {
   useUnfollowUserMutation,
 } from './useFollowQuery';
 
-export const useFriendAction = (userData: User | undefined) => {
+export const useFriendAction = () => {
   const followUserMutation = useFollowUserMutation();
   const unfollowUserMutation = useUnfollowUserMutation();
   const cancelFollowUserMutation = useCancelFollowUserMutation();
   const acceptFollowUserMutation = useAcceptFollowUserMutation();
   const createChatRoom = useCreateOneToOneRoom();
 
-  const handleFollow = () => {
+  const handleFollow = (userData: User | undefined | null) => {
     if (!userData) return;
     followUserMutation.mutate(userData.userId);
   };
 
-  const handleUnfollow = () => {
+  const handleUnfollow = (userData: User | undefined | null) => {
     if (!userData) return;
     unfollowUserMutation.mutate(userData.userId);
   };
 
-  const handleCancelFollow = () => {
+  const handleCancelFollow = (userData: User | undefined | null) => {
     if (!userData) return;
     cancelFollowUserMutation.mutate(userData.userId);
   };
 
-  const handleAcceptFollow = () => {
+  const handleAcceptFollow = (userData: User | undefined | null) => {
     if (!userData) return;
     acceptFollowUserMutation.mutate(userData.userId);
   };
 
-  const handleChat = (routeType: 'push' | 'replace', dissMissCount: number, onClose?: () => void) => {
+  const handleChat = (
+    userData: User | undefined | null,
+    routeType?: 'push' | 'replace',
+    dissMissCount?: number,
+    onClose?: () => void,
+  ) => {
     if (!userData) return;
     createChatRoom.mutate({
       otherUserId: userData.userId,
@@ -45,13 +50,20 @@ export const useFriendAction = (userData: User | undefined) => {
     });
   };
 
-  const getFollowAction = () => {
-    if (userData?.followStatus === 'FRIEND') return { decline: { label: 'Unfollow', onPress: handleUnfollow } };
-    if (userData?.followStatus === 'FOLLOWING') return { secondary: { label: 'Pending', onPress: handleCancelFollow } };
-    if (userData?.followStatus === 'FOLLOWED') return { primary: { label: 'Accept', onPress: handleAcceptFollow } };
+  // 채팅 제외, 친구 상태에 따른 버튼 액션 반환
+  const getFollowAction = (userData: User | undefined | null) => {
+    if (userData?.followStatus === 'FRIEND')
+      return { secondary: { label: 'Following', onPress: () => handleUnfollow(userData) } };
+    if (userData?.followStatus === 'FOLLOWING')
+      return { secondary: { label: 'Requested', onPress: () => handleCancelFollow(userData) } };
+    if (userData?.followStatus === 'FOLLOWED')
+      return {
+        primary: { label: 'Accept', onPress: () => handleAcceptFollow(userData) },
+        decline: { label: 'Decline', onPress: () => handleUnfollow(userData) },
+      };
 
     // 기본값 (아무 관계 아닐 때)
-    return { primary: { label: 'Follow', onPress: handleFollow } };
+    return { primary: { label: 'Follow', onPress: () => handleFollow(userData) } };
   };
 
   return {
